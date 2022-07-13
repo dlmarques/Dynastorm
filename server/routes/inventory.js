825ee7f6ab15702059fe1296f70da7e68eca6d62
dlmarques <daniel.marquesedigital@gmail.com>
2022-07-13 17:58:49 +0100

    inventory - change users skills values after purchase items

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -11,37 +11,90 @@ router.post("/addItem", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   const id = jwt.decode(req.body.token, process.env.JWT_TOKEN);
-  const item = await Inventory.findOne({ id: id });
-  const user = await User.findOne({_id: id})
+  const item = await Inventory.findOne({ itemName: req.body.itemName });
+  const user = await User.findOne({ _id: id });
 
-  if(item && req.body.itemName === item.itemName){
-      try{
-        await Inventory.findByIdAndUpdate({_id : item._id}, {$set : {quantity : item.quantity + req.body.quantity}})
-         if(user){
-          await User.findOneAndUpdate({_id: id}, {$set: {money: user.money - req.body.price}})
-          res.send('inserted')
-         } 
-      }catch(err){
-        res.send(err)
+  if (item && req.body.itemName === item.itemName) {
+    await Inventory.findOneAndUpdate(
+      { itemName: req.body.itemName },
+      { $set: { quantity: item.quantity + req.body.quantity } }
+    );
+    if (user) {
+      await User.findOneAndUpdate(
+        { _id: id },
+        { $set: { money: user.money - req.body.price } }
+      );
+      const percentage = req.body.percentage;
+      if (req.body.itemSkill === "magic") {
+        await User.findOneAndUpdate(
+          { _id: id },
+          { $set: { magic: user.magic + user.magic * percentage } }
+        );
+      } else if (req.body.itemSkill === "speed") {
+        await User.findOneAndUpdate(
+          { _id: id },
+          { $set: { speed: user.speed + user.speed * percentage } }
+        );
+      } else if (req.body.itemSkill === "stamina") {
+        await User.findOneAndUpdate(
+          { _id: id },
+          { $set: { stamina: user.stamina + user.stamina * percentage } }
+        );
+      } else if (req.body.itemSkill === "strength") {
+        await User.findOneAndUpdate(
+          { _id: id },
+          { $set: { strength: user.strength + user.strength * percentage } }
+        );
+      } else {
+        res.send("error");
       }
-  }else{
-  try {
-    //Create new item
-    const newItem = new Inventory({
-      id: id,
-      itemName: req.body.itemName,
-      itemSkill: req.body.itemSkill,
-      quantity: req.body.quantity,
-    });
-    await newItem.save();
-    if(user){
-      await User.findOneAndUpdate({_id: id}, {$set: {money: user.money - req.body.price}})
-      res.send('inserted')
-     } 
-  } catch (err) {
-    res.status(400).send(err);
+      res.send("inserted");
+    }
+  } else {
+    try {
+      //Create new item
+      const newItem = new Inventory({
+        id: id,
+        itemName: req.body.itemName,
+        itemSkill: req.body.itemSkill,
+        quantity: req.body.quantity,
+      });
+      await newItem.save();
+      if (user) {
+        await User.findOneAndUpdate(
+          { _id: id },
+          { $set: { money: user.money - req.body.price } }
+        );
+        const percentage = req.body.percentage;
+        if (req.body.itemSkill === "magic") {
+          await User.findOneAndUpdate(
+            { _id: id },
+            { $set: { magic: user.magic + user.magic * percentage } }
+          );
+        } else if (req.body.itemSkill === "speed") {
+          await User.findOneAndUpdate(
+            { _id: id },
+            { $set: { speed: user.speed + user.speed * percentage } }
+          );
+        } else if (req.body.itemSkill === "stamina") {
+          await User.findOneAndUpdate(
+            { _id: id },
+            { $set: { stamina: user.stamina + user.stamina * percentage } }
+          );
+        } else if (req.body.itemSkill === "strength") {
+          await User.findOneAndUpdate(
+            { _id: id },
+            { $set: { strength: user.strength + user.strength * percentage } }
+          );
+        } else {
+          res.send("error");
+        }
+        res.send("inserted");
+      }
+    } catch (err) {
+      res.status(400).send(err);
+    }
   }
-}
 });
 
 module.exports = router;
--- web/src/components/App/Modules/Store/components/Item.js
+++ web/src/components/App/Modules/Store/components/Item.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import styles from "./item.module.scss";
-import axios from 'axios'
-import { useDispatch } from 'react-redux'
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
-import { shopSliceActions } from '../../../../../store/shop/shopSlice'
+import { shopSliceActions } from "../../../../../store/shop/shopSlice";
 import Counter from "./Counter";
 import Product from "./Product";
 
@@ -15,37 +15,40 @@ const Item = ({ img }) => {
     skill: null,
     percentage: null,
     price: null,
-  })
+  });
 
   const buyItem = async () => {
-    const authToken = localStorage.getItem('authToken')
+    const authToken = localStorage.getItem("authToken");
     const price = item.price * quantity;
     let response = axios.post("http://localhost:3001/api/inventory/addItem", {
       token: authToken,
       itemName: item.name,
       itemSkill: item.skill,
       quantity: quantity,
-      price: price
+      price: price,
+      percentage: item.percentage/100,
     });
 
     try {
       const registerResponse = await response;
-      dispatch(shopSliceActions.buy())
+      dispatch(shopSliceActions.buy());
       if (registerResponse.data === "success") {
-        console.log('success')
+        console.log("success");
       }
-      setQuantity(1)
+      setQuantity(1);
     } catch (err) {
       console.error(err);
     }
-  }
- 
+  };
+
   return (
     <div className={styles.item}>
       <Product img={img} quantity={quantity} item={item} setItem={setItem} />
       <Counter quantity={quantity} setQuantity={setQuantity} />
       <div className={styles.buy}>
-      <button className={styles.buy} onClick={buyItem}>Buy</button>
+        <button className={styles.buy} onClick={buyItem}>
+          Buy
+        </button>
       </div>
     </div>
   );
