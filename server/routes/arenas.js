51569024b72218183eb3b4d1bd3afcc14f62337b
dlmarques <daniel.marquesedigital@gmail.com>
2022-08-03 12:45:56 +0100

    arenas and elo logic

--- server/routes/arenas.js
+++ server/routes/arenas.js
@@ -36,4 +36,74 @@ router.post("/getEnemy", async (req, res) => {
   }
 });
 
+router.post("/attackEnemy", async (req, res) => {
+  const id = jwt.decode(req.body.token, process.env.JWT_TOKEN);
+  const enemy = await User.findById(req.body.id);
+  const user = await User.findById(id)
+  //if user and enemy exists
+  if(user && enemy){
+    //average skills
+    const enemyAverageSkills = enemy.strength + enemy.magic + enemy.armor + enemy.magicResist;
+    const userAverageSkills = user.strength + user.magic + user.armor + user.magicResist;
+    //if user is stronger than enemy
+    if(enemyAverageSkills < userAverageSkills){
+      //overall difference between user and enemy
+      const diff = userAverageSkills - enemyAverageSkills;
+      //if diff is equal or more than 50%
+      if(diff >= userAverageSkills * 0.5){
+        const userProb = [1, 2, 3, 4, 5, 6, 7, 8];
+        const enemyProb = [9 , 10];
+        const random = Math.floor(Math.random() * 10 + 1);
+        if(userProb.includes(random)){
+          res.send("user win")
+        }else if(enemyProb.includes(random)){
+          res.send("enemy win")
+        }
+        
+      }else{
+        const userProb = [1, 2, 3, 4, 5, 6, 7];
+        const enemyProb = [8, 9 , 10];
+        const random = Math.floor(Math.random() * 10 + 1);
+        if(userProb.includes(random)){
+          res.send("user win")
+        }else if(enemyProb.includes(random)){
+          res.send("enemy win")
+        }
+      }
+       //if average skills are equal 
+    }else  if(enemyAverageSkills === userAverageSkills){
+        const userProb = [1, 2, 3, 4, 5];
+        const enemyProb = [6, 7, 8, 9, 10];
+        const random = Math.floor(Math.random() * 10 + 1);
+        if(userProb.includes(random)){
+          res.send("user win")
+        }else if(enemyProb.includes(random)){
+          res.send("enemy win")
+        }
+    }else if (enemyAverageSkills > userAverageSkills){
+      const diff = enemyAverageSkills - userAverageSkills;
+      if(diff >= enemyAverageSkills * 0.5){
+        const enemyProb = [1, 2, 3, 4, 5, 6, 7, 8];
+        const userProb = [9 , 10];
+        const random = Math.floor(Math.random() * 10 + 1);
+        if(enemyProb.includes(random)){
+          res.send("enemy win")
+        }else if(userProb.includes(random)){
+          res.send("user win")
+        }
+      }else {
+        const enemyProb = [1, 2, 3, 4, 5, 6, 7];
+        const userProb = [8, 9 , 10];
+        const random = Math.floor(Math.random() * 10 + 1);
+        if(userProb.includes(random)){
+          res.send("user win")
+        }else if(enemyProb.includes(random)){
+          res.send("enemy win")
+        }
+      }
+    }
+  }
+
+});
+
 module.exports = router;
