001429e4830243f7b9f79b44103eb8397ef64d79
dlmarques <daniel.marquesedigital@gmail.com>
2022-10-02 14:53:07 +0100

    fix profile bar money

--- web/src/components/Layout/Main/Main.jsx
+++ web/src/components/Layout/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import Container from "../../App/Components/Notifications/components/Container";
--- web/src/components/Layout/Main/components/Profile.jsx
+++ web/src/components/Layout/Main/components/Profile.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import { useSelector } from "react-redux";
 import Notifications from "../../../App/Components/Notifications/Notifications";
 import styles from "./profile.module.scss";
-import { convert } from "../../../../utils/numbersConvert";
-import Tooltip from "../../../App/Components/Tooltip/Tooltip";
 
 const Profile = () => {
-  const [isVisible, setIsVisible] = useState();
   const user = useSelector((state) => state.user.user);
 
   return (
@@ -18,14 +14,7 @@ const Profile = () => {
             <Notifications />
             <div className={styles.info}>
               <span>
-                <p
-                  onMouseEnter={() => setIsVisible(true)}
-                  onMouseLeave={() => setIsVisible(false)}
-                  className={styles.money}
-                >
-                  ${user.money}
-                </p>
-                {isVisible && <Tooltip info={`$${user.money}`} />}
+                <p className={styles.money}>${Math.floor(user.money)}</p>
               </span>
 
               <img src={user && user.avatar} alt="avatar" />
