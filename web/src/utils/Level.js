93163829599bcde034d0d9286da513ba5e478d9e
dlmarques <daniel.marquesedigital@gmail.com>
2022-10-09 18:11:09 +0100

    improve levels logic and rewards

--- web/src/utils/Level.js
+++ web/src/utils/Level.js
@@ -1,35 +1,35 @@
 export const setLevel = (xp) => {
-  if (xp <= 100) {
+  if (xp <= 200) {
     return 1;
-  } else if (xp <= 200) {
-    return 2;
-  } else if (xp <= 300) {
-    return 3;
   } else if (xp <= 400) {
-    return 4;
-  } else if (xp <= 500) {
-    return 5;
+    return 2;
   } else if (xp <= 600) {
+    return 3;
+  } else if (xp <= 800) {
+    return 4;
+  } else if (xp <= 1000) {
+    return 5;
+  } else if (xp <= 1200) {
     return 6;
-  } else if (xp <= 700) {
+  } else if (xp <= 1400) {
     return 7;
   }
 };
 
 export const setTier = (xp) => {
-  if (xp <= 100) {
+  if (xp <= 200) {
     return "Servant";
-  } else if (xp <= 200) {
-    return "Yeomen";
-  } else if (xp <= 300) {
-    return "Freemen";
   } else if (xp <= 400) {
-    return "Peasants";
-  } else if (xp <= 500) {
-    return "Knight";
+    return "Yeomen";
   } else if (xp <= 600) {
+    return "Freemen";
+  } else if (xp <= 800) {
+    return "Peasants";
+  } else if (xp <= 1000) {
+    return "Knight";
+  } else if (xp <= 1200) {
     return "King";
-  } else if (xp <= 700) {
+  } else if (xp <= 1400) {
     return "Pope";
   }
 };
